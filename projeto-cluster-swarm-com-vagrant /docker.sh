8940947e4571d6fad189043d159794ea671de703
Matheus Fernandes Santos <matheusfsantos.ti@gmail.com>
2023-08-29 17:44:28 -0300

    Delete projeto-cluster-swarm-com-vagrant  directory

--- projeto-cluster-swarm-com-vagrant /docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -fsSL https://get.docker.com | sudo bash
-sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo usermod -aG docker vagrant
